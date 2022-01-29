782e4ae8d62a1d99729438e36f5cdbbfe46b82ac
kamoto <kamototakahiro@kamototakahironoMacBook-Air.local>
2022-01-29 17:28:47 +0900

    ユーザー一覧とルーティングの追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">ホーム</router-link> |
+      <router-link to="/chat">チャット</router-link>
       
     </div>
     <router-view/>
